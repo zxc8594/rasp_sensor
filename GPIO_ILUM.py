8f5bdc80a7eb91d1256439b4742e933c92a27878
lee <zxc8594@naver.com>
2019-12-15 16:02:27 +0000

    ilum added

--- GPIO_ILUM.py
+++ GPIO_ILUM.py
@@ -1,6 +1,6 @@
 # coding : utf-8
 
-import RPi.GPIO as GPIO
+import wiringpi as wp
 import time
 
 CS_MCP3208 = 8
@@ -9,8 +9,27 @@ SPI_SPEED = 1000000
 
 def ReadMcp3208ADC(adcChannel):
 	nAdcValue = 0
-	buff[0] = 0x06 | ((adcChannel & 0x07) >> 2)
+	
+	wp.digitalWrite(CS_MCP3208, 0)
+	retlen, retdata = wp.wiringPiSPIDataRW(SPI_CHANNEL, bytes([6,0,0]))
+	r1 = retdata[1]
+	r1 = 0x0F & r1
+	nAdcValue = (r1 << 8) | retdata[2]
+	wp.digitalWrite(CS_MCP3208, 1)
+	return nAdcValue
 
 # main
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+nCdsChannel = 0
+nPhotoCellChannel = 1
+nCdsValue = 0
+nPhotoCellValue = 0
+
+wp.wiringPiSetupGpio()
+wp.wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED)
+wp.pinMode(CS_MCP3208, wp.OUTPUT)
+
+while True:
+	nCdsValue = ReadMcp3208ADC(nCdsChannel)
+	print('LIGHT = %d Lux' % nCdsValue)
+	time.sleep(0.5)
+
